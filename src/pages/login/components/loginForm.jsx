74b2f3ce43dc88d056a69d013e0556d952b3d771
directorBae <bjw020615@gmail.com>
2024-04-01 22:16:21 +0900

    LoginView Component

---
+++ src/pages/login/components/loginForm.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import styled from "styled-components";
+
+const TitleStyle = styled.div`
+  margin: 5% 0;
+  font-family: Beaufort;
+  width: 100%;
+  font-weight: 900;
+  text-align: center;
+  letter-spacing: 0.03em;
+  color: #ffffff;
+  text-shadow: 0px 0px 13px rgba(255, 255, 255, 0.5);
+  font-size: 5em;
+`;
+
+const LoginForm = () => {};
+
+export default LoginForm;
