31d283da2098be99a29fcd1421ea4839711ab4f2
Jeongwon, Bae <bjw020615@gmail.com>
2024-04-04 16:49:24 +0900

    Routes

--- src/App.js
+++ src/App.js
@@ -4,17 +4,20 @@ import MatchView from "./pages/match/view";
 import LoginView from "./pages/login/view";
 import ResultView from "./pages/result/view";
 import HomeViewModel from "./pages/home/vm";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 
 function App() {
   const homevm = new HomeViewModel();
 
   return (
-    <div>
-      <HomeView vm={homevm} />
-      <LoginView />
-      <MatchView />
-      <ResultView />
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeView vm={homevm} />} />
+        <Route path="/user" element={<LoginView />} />
+        <Route path="/match" element={<MatchView />} />
+        <Route path="/result" element={<ResultView />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
